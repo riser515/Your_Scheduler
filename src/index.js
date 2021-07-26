c8b962ad39d3b1e6f730bb4754b93410909535e0
Khushi <sparker.star001@gmail.com>
2021-07-26 20:47:20 +0530

    Added JS for styling elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import './styles.css';
 
-console.log("Your Personal Scheduler");
+let tasks = [];
+const tasksRoot = document.querySelector('.tasks');
+const list = tasksRoot.querySelector('.tasks__list');
+const form = document.forms.tasks;
+const input = form.elements.task;
+
+function renderTask(tasks){
+    let taskString = '';
+    tasks.forEach((task, index) => {
+        taskString += `
+        <li data-id="${index}"${task.complete ? 'class="task--complete"' : ''}>
+          <input type="checkbox"${task.complete ? 'checked' : ''} />
+          <span>${task.text}</span>
+          <button type="button"></button>
+        </li>
+        `
+    });
+    list.innerHTML = taskString;
+}
+
+function addTask(e){
+    e.preventDefault();
+    const text = input.value.trim();
+    const complete = false;
+    tasks = [
+        ...tasks,
+        {
+            text,
+            complete
+        }
+    ];
+    renderTask(tasks);
+    input.value = '';
+}
+
+function updateTask(e){
+    const id = parseInt(e.target.parentNode.getAttribute('data-id'), 10);
+    let complete = e.target.checked;
+    tasks = tasks.map((task,index) => {
+        if(index === id){
+            return{
+                ...task,
+                complete
+            };
+        }
+        return task;
+    });
+    renderTask(tasks);
+}
+
+function init(){
+    form.addEventListener('submit', addTask);
+    list.addEventListener('change', updateTask);
+}
+
+init();
